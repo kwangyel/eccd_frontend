dab66ea4eebb66469ab7a6f932937a0dab65fab6
kinley wangyel <kwangyel@gmail.com>
2025-08-10 14:06:46 +0600

    add LandingV2Component with template, styles, and tests

---
+++ src/app/presentations/public/landing-v2/landing-v2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-v2',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-v2.component.html',
+  styleUrl: './landing-v2.component.scss'
+})
+export class LandingV2Component {
+
+}
